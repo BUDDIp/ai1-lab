ebaffad709d76ed9184eb7fd7947be7784f8928e
Norbert Dembek <dn50957>
2025-10-27 17:14:38 +0100

    labc

--- script.js
+++ script.js
@@ -1,304 +1,176 @@
-class Task{
-    constructor(text, date = "", finished = 0){
-        this.text = text;
-        this.date = date;
-        this.finished = finished;
+let map;
+let marker;
+let width;
+let height;
+
+let placedCorrectly = 0;
+
+function showNotification()
+{
+    Notification.requestPermission();
+
+    if(Notification.permission === "granted")
+    {
+        const notification = new Notification("Congratulations! You made it!");
     }
 }
 
-class ToDoList {
-    constructor(tasks = [])
+function startHTML()
+{
+    map = L.map('map').setView([53.430127, 14.564802], 18);
+    // L.tileLayer.provider('OpenStreetMap.DE').addTo(map);
+    L.tileLayer.provider('Esri.WorldImagery').addTo(map);
+    marker = L.marker([53.430127, 14.564802]).addTo(map);
+    marker.bindPopup("<strong>Hello!</strong><br>This is a popup.");
+
+    let canvas = document.getElementById("rasterMap");
+    let dimensions = map.getSize();
+    width = dimensions.x;
+    height = dimensions.y;
+    canvas.width = width;
+    canvas.height = height;
+
+    // Set target behaviour
+    let targets = document.querySelectorAll(".target");
+
+    for(let target of targets)
     {
-        this.tasks = tasks;   
+        target.style.border = "1px dashed red";
+
+        target.style.width = width/4 + "px";
+        target.style.height = height/4 + "px";
+
+        target.addEventListener("dragenter", function (event){
+            this.style.border = "1px solid #252525";
+        });
+        target.addEventListener("dragexit", function (event){
+            this.style.border = "1px dashed red";
+        });
+        target.addEventListener("drop", function (event){
+            let myPiece = document.querySelector("#" + event.dataTransfer.getData("text"));
+
+            // Sprawdz czy miejsce jest puste
+            if(this.innerHTML === "") {
+                this.style.border = "none";
+                this.appendChild(myPiece);
+
+                // Sprawdz czy element jest na swoim miejscu
+                if(myPiece.id.slice(1) === this.id.slice(1))
+                {
+                    myPiece.draggable = false;
+                    this.style.border = "0 dashed red";
+                    placedCorrectly++;
+
+                    console.log("Poprawnie, umieszczono " + placedCorrectly.toString() + " z 16 puzzli");
+                    if(placedCorrectly === 16)
+                    {
+                        console.log("Ukonczono!");
+                        showNotification();
+                    }
+                }
+            }
+
+        }, false    );
+        target.addEventListener("dragover",function (event){
+           event.preventDefault();
+        });
+    }
+}
+function getLocation()
+{
+    if (! navigator.geolocation) {
+        console.log("No geolocation.");
     }
 
-    createHtmlElement(_type, _class, _id = -1)
-    {
-        var element = document.createElement(_type,);
+    navigator.geolocation.getCurrentPosition(position => {
+        console.log(position);
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
 
-        if(_class != "")
-            element.className = _class;
+        map.setView([lat, lon]);
+    }, positionError => {
+        console.error(positionError);
+    });
+}
 
-        if(_id >= 0)
-            element.id = _id;
+function saveMapAsImage()
+{
+    leafletImage(map, function (err, canvas) {
+        // here we have the canvas
+        let rasterMap = document.getElementById("rasterMap");
+        let rasterContext = rasterMap.getContext("2d");
 
-        return element;
+        rasterContext.drawImage(canvas, 0, 0, width, height);
+
+        sliceImage();
+    });
+}
+
+function sliceImage()
+{
+    // Wyczysc kontener
+    const container = document.getElementById("pomieszane");
+    container.innerHTML = "";
+
+    // Okresl rozmiary czesci
+    const sw = width / 4;
+    const sh = height / 4;
+
+    // zapisz informacje o kazdym elemencie obrazu w tablicy
+    const arrayOfImages = [];
+    for (let i = 0; i < 4; i++) {
+        const offset_y = sh * i;
+
+        for (let j = 0; j < 4; j++) {
+            // utworz element canvas
+
+            const offset_x = sw * j;
+
+            const info = ["C" + (i * 4 + j), offset_x, offset_y];
+            arrayOfImages.push(info);
+        }
     }
 
-    draw()
-    {
-        let con = document.getElementById("container");
-        con.innerHTML = "";
+    //shuffle
+    let size = arrayOfImages.length;
 
-        let id = 0;
-        for(let task of this.tasks)
+    for(let i  = 0; i < size; i++)
+    {
+         let rand = Math.floor(Math.random() * size);
+         const temp = arrayOfImages[i];
+         arrayOfImages[i] = arrayOfImages[rand];
+         arrayOfImages[rand] = temp;
+    }
+
+    // Ryswoanie elementow
+    leafletImage(map, function (err, canvas) {
+        for(let i = 0; i < size; i++)
         {
-            let _div = this.createHtmlElement("div", "entry", id);
-
-            let _checkbox = this.createHtmlElement("div", "checkbox");
-            _checkbox.onclick = function(event) {
-                cross(event.target.parentElement);
-            }
+            const pieceInfo = arrayOfImages[i];
+            const id = pieceInfo[0];
+            const offset_x = pieceInfo[1];
+            const offset_y = pieceInfo[2];
 
 
-            let _info = this.createHtmlElement("div", "info");
+            const image = document.createElement("canvas");
+            image.width = width;
+            image.height = height;
+            image.classList.add("piece");
+            image.id = id;
+            image.draggable = true;
+            image.style.width = sw + "px";
+            image.style.height = sh + "px";
 
-            let _text = this.createHtmlElement("div", "text");
-            let searchbar = document.getElementById("searchBar");
-            if(searchbar.value != "" && searchbar.value.length >= 2)
+            let rasterContext = image.getContext("2d");
+
+            rasterContext.drawImage(canvas, offset_x, offset_y, sw, sh, 0, 0, width, height);
+
+            container.append(image);
+
+            image.addEventListener("dragstart", function (event)
             {
-                let textPrompt = searchbar.value;
-                let position = task.text.search(new RegExp(textPrompt, "i"));
-                if(position < 0)
-                    continue;
-
-                let span = this.createHtmlElement("span", "HL");
-                let hltext = task.text.substr(position, searchbar.value.length);
-                span.append(hltext);
-                _text.append(task.text.substr(0, position));
-                _text.append(span);
-                _text.append(task.text.substr(position + searchbar.value.length, task.text.length));
-            }
-            else
-                _text.innerText = task.text;
-
-            _text.addEventListener("click", (e) => {
-                let entry = e.target.parentElement.parentElement;
-                let id = parseInt(entry.getAttribute("id"));
-
-                if(todo.tasks[id].finished == 0)
-                {
-                    let inputField = document.createElement("input");
-                    let text = _text.innerText;
-                    inputField.value = text;
-                    inputField.addEventListener("focusout", (e) => {
-                        saveChanges(e, "text");
-                    });
-            
-                    e.target.replaceWith(inputField);
-                    inputField.focus();
-                }
-            });
-
-            let _date = this.createHtmlElement("div", "date");
-            _date.innerText = task.date;
-            _date.addEventListener("click", (e) => {
-                let entry = e.target.parentElement.parentElement;
-                let id = parseInt(entry.getAttribute("id"));
-
-                if(todo.tasks[id].finished == 0)
-                {
-                    let inputField = document.createElement("input");
-                    inputField.type = "date";
-                    let text = _date.innerText;
-                    inputField.value = text;
-                    inputField.addEventListener("focusout", (e) => {
-                        saveChanges(e, "date");
-                    });
-            
-                    e.target.replaceWith(inputField);
-                    inputField.focus();
-                }
-            });
-
-
-            let _line = this.createHtmlElement("div", "line");
-
-
-            let _removeB = this.createHtmlElement("div", "removeB");
-            _removeB.onclick = function(event) {
-                todo.deleteElement(event.target.parentElement.getAttribute("id"));
-            }
-
-            _info.append(_text);
-            _info.append(_date);
-            _info.append(_line);
-
-            _div.append(_checkbox);
-            _div.append(_info);
-            _div.append(_removeB);
-            con.append(_div);
-
-            setColors(_div, task.finished);
-
-            id = id + 1;
+                event.dataTransfer.setData("text", this.id)
+            })
         }
-
-        this.save();
-    }
-
-    addElement(text, date)
-    {
-        this.tasks.push(new Task(text, date));
-    }
-
-    deleteElement(idx)
-    {
-        this.tasks.splice(idx, 1);
-        this.draw();
-    }
-
-    save()
-    {
-        const json = JSON.stringify(this.tasks);      
-        localStorage.setItem("todo", json);
-    }
-}
-
-var todo = new ToDoList();
-var currentDate = new Date(); 
-
-function start()
-{
-    if(localStorage.getItem("todo"))
-    {
-        let json = localStorage.getItem("todo");
-        let tasks = JSON.parse(json);
-        todo.tasks = tasks;
-    }
-
-    todo.draw();
-}
-
-
-function addElement()
-{
-    let textF = document.getElementById("textField");
-    let dateF = document.getElementById("dateField");
-    let text = textF.value;
-    let date = dateF.value;
-
-    textF.value = "";
-    dateF.value = "";
-
-    if(date != "")
-    {
-        const date_elements = date.split("-");
-        let day = parseInt(date_elements[2]);
-        let month = parseInt(date_elements[1]);
-        let year = parseInt(date_elements[0]);
-
-        date = day + "-" + month + "-" + year;
-
-        if(year == currentDate.getFullYear())
-        {
-            if(month == currentDate.getMonth() + 1)
-            {
-                if(day < currentDate.getDate())
-                    return;
-            }
-            else if(month < currentDate.getMonth())
-                return;
-        }
-        else if(year < currentDate.getFullYear())
-            return;
-    }
-
-    if(text.length < 3 || text.length > 255)
-    {
-        textF.value = "Nie poprawna ilosc znakow";
-        return;
-        // alert("Task musi miec przynajmniej 3 znaki!");   
-    }
-
-    todo.addElement(text, date);
-    todo.draw();
-}
-
-function focusText(text)
-{
-    document.getElementById(text).select()
-}
-
-function cross(e)
-{
-    let entry = e;
-    let line = entry.getElementsByClassName("line")[0];
-
-    let info = entry.querySelector(".info");
-    let checkbox = entry.querySelector(".checkbox");
-
-    if(todo.tasks[e.getAttribute("id")].finished == 1)
-    {
-        checkbox.style.backgroundColor = "rgb(218, 217, 225)";
-        info.style.color = 'rgb(230, 230, 230)';
-        line.style.visibility = "hidden";
-        todo.tasks[e.getAttribute("id")].finished = 0;
-    }
-    else
-    {
-        checkbox.style.backgroundColor = "rgb(95, 83, 154)";
-        info.style.color = "rgb(161, 159, 159)";
-        line.style.visibility = "visible";
-        todo.tasks[e.getAttribute("id")].finished = 1;
-    }
-
-    todo.save();
-}
-
-function setColors(e, finished) {
-
-    let entry = e;
-    let line = entry.getElementsByClassName("line")[0];
-
-    let info = entry.querySelector(".info");
-    let checkbox = entry.querySelector(".checkbox");
-
-    if(finished == 0)
-    {
-        checkbox.style.backgroundColor = "rgb(218, 217, 225)";
-        info.style.color = 'rgb(230, 230, 230)';
-        line.style.visibility = "hidden";
-    }
-    else
-    {
-        checkbox.style.backgroundColor = "rgb(95, 83, 154)";
-        info.style.color = "rgb(161, 159, 159)";
-        line.style.visibility = "visible";
-    }
-}
-
-function changedSearchPrompt(){
-    todo.draw();
-}
-
-function saveChanges(e, type) {
-    let entry = e.target.parentElement.parentElement;
-    let id = parseInt(entry.getAttribute("id"));
-
-    let text = e.target.value;
-    if(type == "text")
-    {
-        if(text.length >= 3)
-            todo.tasks[id].text = text;
-    }
-    else
-    {
-        if(text != "")
-        {    
-            const date_elements = text.split("-");
-            let day = parseInt(date_elements[2]);
-            let month = parseInt(date_elements[1]);
-            let year = parseInt(date_elements[0]);
-
-            text = day + "-" + month + "-" + year;
-
-            if(year == currentDate.getFullYear())
-            {
-                if(month == currentDate.getMonth() + 1)
-                {
-                    if(day < currentDate.getDate())
-                        return todo.draw();
-                }
-                else if(month < currentDate.getMonth())
-                    return todo.draw();
-            }
-            else if(year < currentDate.getFullYear())
-                return todo.draw();
-
-            todo.tasks[id].date = text;
-        }
-    }
-
-    todo.draw();
+    });
 }
